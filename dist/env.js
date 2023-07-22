78f1c3ee181602873a72befe46ee4f7690680e16
Fabian Ahammer <tf3kik@gmail.com>
2023-07-22 10:47:48 +0200

    rm dist

--- dist/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_INITDB_ROOT_USERNAME = exports.MONGO_INITDB_ROOT_PASSWORD = exports.DB_NAME = exports.DB_URL = void 0;
-exports.DB_URL = "mongodb://localhost:27017";
-exports.DB_NAME = "skybitches";
-exports.MONGO_INITDB_ROOT_PASSWORD = "AFA";
-exports.MONGO_INITDB_ROOT_USERNAME = "AFA";
--- routes/routes.ts
+++ routes/routes.ts
@@ -1,8 +1,5 @@
-export function setupRoutes(app: ) {
-	
-    app.post("/login", (req: any, res: any) => {
-
-    });
+export function setupRoutes(app: Express): void {
+	app.post("/login", (req: any, res: any) => {});
 
 	app.post("/createuser", (req: any, res: any) => {
 		res.send(req.body);
